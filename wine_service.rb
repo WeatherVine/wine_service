69c7684c331887d9a54232e1e6bb31d1b55cb200
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-16 13:09:16 -0600

    Make add method call to get /

--- wine_service.rb
+++ wine_service.rb
@@ -1,5 +1,29 @@
 require 'sinatra'
+require 'faraday'
+require 'json'
+require './services/wine'
 
   get '/' do
-    'Hello World!!!'
+    x = WineService.collection_wines
+    require "pry"; binding.pry
   end
+
+    # @page = 0
+
+    # def conn
+    #   @conn = Faraday.new(
+    #     'https://quiniwine.com'
+    #   )
+    # end
+    #
+    # def location_and_vintage
+    #   response = conn.get("/api/pub/wineKeywordSearch/napa%20napa%20valley/#{@page}")
+    #   search = JSON.parse(response.body, symbolize_names: true)
+    # end
+
+
+  #
+  #
+  # def increase_page
+  #   @page += 1
+  # end
