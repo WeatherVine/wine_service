32ef306cc47863bb1a81f3d5c9385c4be64a4047
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-18 14:47:25 -0600

    Auto generated from adding figaro gem

--- wine_service.rb
+++ wine_service.rb
@@ -4,6 +4,7 @@ require 'json'
 require './services/wine'
 require './facades/wine_facade'
 require './serializers/wine_serializer'
+require './serializers/w_serializer'
 require 'pry'
 require 'sinatra/json'
 
@@ -11,5 +12,14 @@ require 'sinatra/json'
     wines = WineFacade.wine_objects(params[:location], params[:vintage])
 
     body WinesSerializer.new(wines).serialized_json
+    status 200
+  end
+
+  get '/wine-single' do
+    wine = WineFacade.wine_single(params[:id])
+
+    require "pry"; binding.pry
+
+    body WSerializer.new(wine).serialized_json
     status 200
   end
