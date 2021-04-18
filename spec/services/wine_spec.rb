e0a2f29917d7450f09b50e54f7d5dbfe910c842e
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-18 17:33:00 -0600

    Add sad path and edge case testing for /wine-data and /wine-single API calls

--- spec/services/wine_spec.rb
+++ spec/services/wine_spec.rb
@@ -13,9 +13,10 @@ describe 'Wine API' do
     Sinatra::Application
   end
 
-  xit "obtains a collection of wine objects" do
+  it 'obtains a collection of wine objects' do
     VCR.use_cassette('collection_wine') do
       get '/wine-data?location=napa valley&vintage=2008'
+
       expect(last_response).to be_ok
 
       wines = JSON.parse(last_response.body, symbolize_names: true)
@@ -36,34 +37,122 @@ describe 'Wine API' do
     end
   end
 
-  it "obtains a single wine" do
-    get '/wine-single?id=5a7a782f43780f1b2595a471'
+  it 'collection wine returns an error if location and vintage query params are missing' do
+    get '/wine-data'
 
     expect(last_response).to be_ok
 
-    wine = JSON.parse(last_response.body, symbolize_names: true
+    error = JSON.parse(last_response.body, symbolize_names: true)
 
-    expect(wine).to be_a(Hash)
-    expect(wine[:data].length).to eq(3)
-    expect(wine[:data][:attributes]).to have_key(:api_id)
-    expect(wine[:data][:attributes][:api_id]).to be_a(String)
-    expect(wine[:data][:attributes]).to have_key(:id)
-    expect(wine[:data][:attributes][:api_id]).to be_a(String)
-    expect(wine[:data][:attributes]).to have_key(:name)
-    expect(wine[:data][:attributes][:name]).to be_a(String)
-    expect(wine[:data][:attributes]).to have_key(:area)
-    expect(wine[:data][:attributes][:area]).to be_a(String)
-    expect(wine[:data][:attributes]).to have_key(:vintage)
-    expect(wine[:data][:attributes][:vintage]).to be_a(String)
-    expect(wine[:data][:attributes]).to have_key(:eye)
-    expect(wine[:data][:attributes][:vintage]).to be_a(String)
-    expect(wine[:data][:attributes]).to have_key(:nose)
-    expect(wine[:data][:attributes][:vintage]).to be_a(String)
-    expect(wine[:data][:attributes]).to have_key(:mouth)
-    expect(wine[:data][:attributes][:vintage]).to be_a(String)
-    expect(wine[:data][:attributes]).to have_key(:finish)
-    expect(wine[:data][:attributes][:vintage]).to be_a(String)
-    expect(wine[:data][:attributes]).to have_key(:overall)
-    expect(wine[:data][:attributes][:vintage]).to be_a(String)
+    expect(error).to be_a(Hash)
+    expect(error[:error]).to be_a(String)
+    expect(error[:error]).to eq('Please provide a location and vintage year')
+  end
+
+  it 'collection wine returns an errror if location query params is missing' do
+    get '/wine-data?vintage=2008'
+
+    expect(last_response).to be_ok
+
+    error = JSON.parse(last_response.body, symbolize_names: true)
+
+    expect(error).to be_a(Hash)
+    expect(error[:error]).to be_a(String)
+    expect(error[:error]).to eq('Please provide a location')
+  end
+
+  it 'collection wine returns an errror if vintage year query params is missing' do
+    get '/wine-data?location=napa valley'
+
+    expect(last_response).to be_ok
+
+    error = JSON.parse(last_response.body, symbolize_names: true)
+
+    expect(error).to be_a(Hash)
+    expect(error[:error]).to be_a(String)
+    expect(error[:error]).to eq('Please provide a vintage year')
+  end
+
+  it 'collection wine returns an errror if vintage year query params is before 1970' do
+    get '/wine-data?location=napa valley&vintage=1969'
+
+    expect(last_response).to be_ok
+
+    error = JSON.parse(last_response.body, symbolize_names: true)
+
+    expect(error).to be_a(Hash)
+    expect(error[:error]).to be_a(String)
+    expect(error[:error]).to eq('Please provide a vintage year between 1970 and 2020')
+  end
+
+  it 'collection wine returns an errror if vintage year query params is after 2021' do
+    get '/wine-data?location=napa valley&vintage=2022'
+
+    expect(last_response).to be_ok
+
+    error = JSON.parse(last_response.body, symbolize_names: true)
+
+    expect(error).to be_a(Hash)
+    expect(error[:error]).to be_a(String)
+    expect(error[:error]).to eq('Please provide a vintage year between 1970 and 2020')
+  end
+
+  it 'obtains a single wine' do
+    VCR.use_cassette('single_wine') do
+      get '/wine-single?id=5a7a782f43780f1b2595a471'
+
+      expect(last_response).to be_ok
+
+      wine = JSON.parse(last_response.body, symbolize_names: true)
+
+      expect(wine).to be_a(Hash)
+      expect(wine[:data].length).to eq(3)
+      expect(wine[:data][:attributes]).to have_key(:api_id)
+      expect(wine[:data][:attributes][:api_id]).to be_a(String)
+      expect(wine[:data][:attributes]).to have_key(:id)
+      expect(wine[:data][:attributes][:api_id]).to be_a(String)
+      expect(wine[:data][:attributes]).to have_key(:name)
+      expect(wine[:data][:attributes][:name]).to be_a(String)
+      expect(wine[:data][:attributes]).to have_key(:area)
+      expect(wine[:data][:attributes][:area]).to be_a(String)
+      expect(wine[:data][:attributes]).to have_key(:vintage)
+      expect(wine[:data][:attributes][:vintage]).to be_a(String)
+      expect(wine[:data][:attributes]).to have_key(:eye)
+      expect(wine[:data][:attributes][:vintage]).to be_a(String)
+      expect(wine[:data][:attributes]).to have_key(:nose)
+      expect(wine[:data][:attributes][:vintage]).to be_a(String)
+      expect(wine[:data][:attributes]).to have_key(:mouth)
+      expect(wine[:data][:attributes][:vintage]).to be_a(String)
+      expect(wine[:data][:attributes]).to have_key(:finish)
+      expect(wine[:data][:attributes][:vintage]).to be_a(String)
+      expect(wine[:data][:attributes]).to have_key(:overall)
+      expect(wine[:data][:attributes][:vintage]).to be_a(String)
+    end
+  end
+
+  it 'single wine returns an error if the id query param is missing' do
+    get '/wine-single'
+
+    expect(last_response).to be_ok
+
+    error = JSON.parse(last_response.body, symbolize_names: true)
+
+    expect(error).to be_a(Hash)
+    expect(error[:error]).to be_a(String)
+    expect(error[:error]).to eq('Please provide an id')
+  end
+
+  it 'single wine returns an error if the id does not return data' do
+    VCR.use_cassette('no_data') do
+      get '/wine-single?id=0'
+
+      expect(last_response).to be_ok
+
+      error = JSON.parse(last_response.body, symbolize_names: true)
+
+      expect(error).to be_a(Hash)
+      expect(error[:error]).to be_a(String)
+      expect(error[:error]).to eq('Not Found')
+    end
   end
 end
