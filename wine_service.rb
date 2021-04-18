6e827e147705d2460cfde01173e374a44e374c07
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-18 17:35:07 -0600

    Add conditional logic to get /wine-single

--- wine_service.rb
+++ wine_service.rb
@@ -9,17 +9,35 @@ require 'pry'
 require 'sinatra/json'
 
   get '/wine-data' do
-    wines = WineFacade.wine_objects(params[:location], params[:vintage])
 
-    body WinesSerializer.new(wines).serialized_json
-    status 200
+    if params.empty?
+      json ({:error => 'Please provide a location and vintage year'})
+    elsif params[:location].nil?
+      json ({:error => 'Please provide a location'})
+    elsif params[:vintage].nil?
+      json ({:error => 'Please provide a vintage year'})
+    elsif !params[:vintage].to_i.between?(1970,2021)
+      json ({:error => 'Please provide a vintage year between 1970 and 2020'})
+    else
+      wines = WineFacade.wine_objects(params[:location], params[:vintage])
+
+      body WinesSerializer.new(wines).serialized_json
+      status 200
+    end
   end
 
   get '/wine-single' do
-    wine = WineFacade.wine_single(params[:id])
 
-    require "pry"; binding.pry
+    if params.empty?
+      json ({:error => 'Please provide an id'})
+    else
+      wine = WineFacade.wine_single(params[:id])
 
-    body WSerializer.new(wine).serialized_json
-    status 200
+      if wine != "Not Found"
+        body WSerializer.new(wine).serialized_json
+        status 200
+      else
+        json ({:error => 'Not Found'})
+      end
+    end
   end
