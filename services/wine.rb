bd1f72b06cd3a2f90e8afe14358e88e1eb291451
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-18 14:49:28 -0600

    Add #wine_single

--- services/wine.rb
+++ services/wine.rb
@@ -6,17 +6,18 @@ class WineService
   @@collection_wines = []
 
   def self.increase_page
-    @@page += 1
+    @@page += 10
   end
 
   def self.conn
     conn = Faraday.new(
-      'https://quiniwine.com'
+      url: 'https://quiniwine.com',
+      headers: { 'Authorization': 'Bearer ups39uhsv7zc2a9jvkb1' }
     )
   end
 
   def self.location_and_vintage(location, vintage)
-    response = conn.get("/api/pub/wineKeywordSearch/#{vintage}%20n#{location}/#{@@page}")
+    response = conn.get("/api/pub/wineKeywordSearch/#{vintage}%20#{location}/#{@@page}")
     search = JSON.parse(response.body, symbolize_names: true)
   end
 
@@ -31,7 +32,6 @@ class WineService
     end
   end
 
-
   def self.collection_wines(location, vintage)
     while @@collection_wines.length < 5
       self.wines(location, vintage)
@@ -39,4 +39,18 @@ class WineService
     end
     @@collection_wines
   end
+
+  def self.wine_single(id)
+    # '/api/pub/wineSummary.json?wine_id=5f065fb5fbfd6e17acaad294'
+    response = conn.get('/api/pub/wineSummary.json') do |req|
+      req.params['wine_id'] = id
+    end
+    search = JSON.parse(response.body, symbolize_names: true)
+  end
 end
+
+# Altamura = 5a820b7154f765b05c5bd94a
+# Celani = 5b1588fcedcff42611e5db3c
+# Cameron Hughes = 5205521de817460200000002
+# Grgich Hills = 5a7a782f43780f1b2595a471
+# Altamura = 5a820b7154f765b05c5bd94a
